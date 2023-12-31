5da40cbfa06de7cceec9ee16e1bd6b24d9473349
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-31 13:25:27 +0900

    rename: Add GenerateAkeome utility function

--- src/utils/RandomlyGenerateAkeome.ts
+++
@@ -1,9 +0,0 @@
-import { akeome, languageList } from "@/constants/akeome.const";
-import { AkeomeKeys } from "@/types/Akeome.types";
-
-export const RandomlyGenerateAkeome = () => {
-  const randomIndex = Math.floor(Math.random() * languageList.length);
-  const randomLanguage = languageList[randomIndex] as AkeomeKeys;
-  const akeomeMessage = akeome[randomLanguage];
-  return akeomeMessage;
-};
