7f9bbbaaa3b856bf6d743f82aec1527d6ef63ded
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-31 11:52:46 +0900

    feat: Add Fireworks component

---
+++ src/components/common/Fireworks/Fireworks.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Particles from "react-particles";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import type { Engine } from "tsparticles-engine";
+
+export const Fireworks = () => {
+  const customInit = async (engine: Engine) => {
+    await loadFireworksPreset(engine);
+  };
+
+  const options = {
+    preset: "fireworks",
+  };
+
+  return <Particles options={options} init={customInit} />;
+};
