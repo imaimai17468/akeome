2e44a5b34a07c3fbc7328a6eb4b1dd330b32f53c
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-31 13:15:06 +0900

    Refactor Akeome component and RandomlyGenerateAkeome utility
    This commit refactors the Akeome component by feat: introducing React hooks (useState and useEffect) to manage the akeomeMessage state. It also updates the RandomlyGenerateAkeome utility to import the correct constants and types.

--- src/components/common/Akeome/Akeome.tsx
+++ src/components/common/Akeome/Akeome.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import { RandomlyGenerateAkeome } from "@/utils/RandomlyGenerateAkeome";
+import { useState, useEffect } from "react";
 
 export const Akeome = () => {
-  const akeomeMessage = RandomlyGenerateAkeome();
+  const [akeomeMessage, setAkeomeMessage] = useState("");
+
+  useEffect(() => {
+    setAkeomeMessage(RandomlyGenerateAkeome());
+  }, []);
+  
   return (
     <h1 className="text-white text-8xl font-black animate-bounce">
       {akeomeMessage}
