8569201c54586db131da5011da95ce5c7206f84b
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-31 13:15:15 +0900

    feat: Add language selector to AkeomePage

--- src/components/page/AkeomePage/AkeomePage.tsx
+++ src/components/page/AkeomePage/AkeomePage.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import { Akeome } from "@/components/common/Akeome";
+import { LanguageSelector } from "@/components/common/LanguageSelector";
 
 export const AkeomePage = () => {
   return (
     <div className="z-10">
+      <div className="absolute left-2 top-2">
+        <LanguageSelector onChange={(language) => console.log(language)} />
+      </div>
       <Akeome />
     </div>
   );
